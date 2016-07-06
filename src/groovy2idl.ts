c93f00f2f42f8572048d7742498a5e9c24ae2ece
Aci <wangz@qiezzi.com>
2016-07-06 17:25:37 +0800

    finish groovy method code to idl

--- src/groovy2idl.ts
+++ src/groovy2idl.ts
@@ -27,71 +27,150 @@ export function convert(path: string) {
     var code = fileStream.readFileSync(path + file).toString();
     if (file.lastIndexOf('Facade.groovy') > -1) {
 
-      var itemInterface = {
-        package: '',
-        name: '',
-        doc: '',
-        meta: [{
-          type: '',
-          args: {}
-        }]
-      };
-      itemInterface.meta.pop();
-      var itemMethod = {
-        name: '',
-        doc: '',
-        throws: [{}],
-        return: {},
-        args: [{}]
-      };
-      itemMethod.throws.pop();
-      itemMethod.args.pop();
-      var dicTypes = [''];
-
-      itemInterface.package = code.match(/package((\s*?.*?)*?)\n/g)[0].replace('package ', '').replace('\n', '');
-      var interfaceDoc = code.match(/[\/\*\*|\n]((\s*?.*?)*?)interface/);
-      if(interfaceDoc.length > 2){
-        itemInterface.doc = interfaceDoc[1].replace(/\*|\n|\/| /g, '');
-      }
-      itemInterface.name = code.match(/interface((\s*?.*?)*?)Facade/g)[0].replace('interface ', '');
-
-      var metaes = code.match(/@((\s*?.*?)*?)\n/g);
-      for (var i in metaes) {
-        itemInterface.meta.push({
-          type: rpcPackage + metaes[i],
-          args: {}
-        });
-      }
-
-      var methods = code.match(/\{((\s*?.*?)*?)\}/g);
-      for (var i in methods) {
-
-      }
-
+      var itemInterface = getInterface(code);
       idl.interfaces.push(itemInterface);
 
-    } else if (file.lastIndexOf('DTO.groovy') > -1){
+    } else if (file.lastIndexOf('DTO.groovy') > -1) {
 
-      var itemType = {
-        package: '',
-        name: '',
-        doc: '',
-        meta: {},
-        properties: [{}]
-      };
-      itemType.properties.pop();
-
-      itemType.package = code.match(/package((\s*?.*?)*?)\n/g)[0].replace('package ', '');
-      var typeDoc = code.match(/[\/\*\*|\n]((\s*?.*?)*?)class/);
-      if(typeDoc.length > 2){
-        itemType.doc = typeDoc[1].replace(/\*|\n|\/| /g, '');
-      }
-      itemType.name = code.match(/class((\s*?.*?)*?)DTO/g)[0].replace('class ', '');
-
+      var itemType = getType(code);
       idl.types.push(itemType);
-      
+
     }
 
   }
   console.log(JSON.stringify(idl));
+}
+
+function getInterface(code: string): any {
+
+  var itemInterface = {
+    package: '',
+    name: '',
+    doc: '',
+    meta: [{
+      type: '',
+      args: {}
+    }],
+    methods: [{}]
+  };
+  itemInterface.meta.pop();
+  itemInterface.methods.pop();
+  var itemMethod = {
+    name: '',
+    doc: '',
+    throws: [{}],
+    return: {},
+    args: [{}]
+  };
+  itemMethod.throws.pop();
+  itemMethod.args.pop();
+  var dicTypes = [''];
+
+  itemInterface.package = code.match(/package((\s*?.*?)*?)\n/)[0].replace(/package |\n/g, '');
+  itemInterface.name = code.match(/interface((\s*?.*?)*?)Facade/)[0].replace('interface ', '');
+  var interfaceDoc = code.match(/\/\*\*((\s*?.*?)*?)interface/);
+  if (interfaceDoc && interfaceDoc.length > 2) {
+    itemInterface.doc = interfaceDoc[1].substring(0,interfaceDoc[1].indexOf('*/')).replace(/\*|\n|\/| |/g, '');
+  } else {
+    throw new Error(`no comment for the interface: ${itemInterface.name}`);
+  }
+
+  var metaes = code.match(/\*\/((\s*?.*?)*?)interface/);
+  if(metaes){
+    metaes = metaes[0].match(/@((\s*?.*?)*?)\n/g);
+  }
+  for (var i in metaes) {
+    itemInterface.meta.push({
+      type: rpcPackage + metaes[i].replace(/\n|@/g,''),
+      args: {}
+    });
+  }
+
+  var methodCode = code.match(/\{((\s*?.*?)*?)\}/g);
+  if(!methodCode){
+    throw new Error(`no methods for the interface: ${ itemInterface.name }`);
+  }
+  var methods = methodCode[0].match(/\/\*\*((\s*?.*?)*?)\)/g);
+  for (var i in methods) {
+    var method = getMethod(methods[i]);
+    itemInterface.methods.push(method);
+  }
+
+  return itemInterface;
+}
+
+function getMethod(code: string): any {
+  var method = {
+    name: '',
+    doc: '',
+    throws: [{}],
+    return: {
+      type: '',
+      doc: ''
+    },
+    args: [{}]
+  }
+
+  method.args.pop();
+
+  var methodName = code.match(/[a-zA-Z](w?.)*\(/);
+  if(!methodName){
+    throw new Error(`code format error: ${ code }`);
+  }
+  method.name = methodName[0].match(/[ ](w?.)*\(/)[0].replace(/ |\(/g,'');
+  method.return.type = methodName[0].match(/[a-zA-Z](w?.)* /)[0].replace(' ','');
+
+  var doces = code.match(/\*((\s*?.*?)*?)\n/g)
+  if(!doces){
+    throw new Error(`no document for the method: ${ method.name }`);
+  }
+  method.doc = doces[1].replace(/\* |\n/g,'');
+
+  var docReturn = code.match(/\@return((\s*?.*?)*?)\n/);
+  method.return.doc = docReturn[0].replace(/@return |\n/g,'');
+
+  var argsDoc = code.match(/\@param((\s*?.*?)*?)\n/g);
+  var args = code.match(/\(((\s*?.*?)*?)\)/g)[0].replace(/\(|\)/g,'').split(',');
+ 
+  var methodArg = {
+    name: '',
+    type: '',
+    doc: ''
+  }
+  for(var i in args){
+    var tmp = args[i].split(' ');
+    methodArg.type = tmp[0];
+    methodArg.name = tmp[1];
+    methodArg.doc = argsDoc[i].replace(/@param |\n/g, '');
+    method.args.push(methodArg);
+  }
+
+  return method;
+}
+
+
+function getType(code: string): any {
+
+  var itemType = {
+    package: '',
+    name: '',
+    doc: '',
+    meta: {},
+    properties: [{}]
+  };
+  itemType.properties.pop();
+
+  itemType.package = code.match(/package((\s*?.*?)*?)\n/g)[0].replace(/package |\n/g, '');
+  var typeDoc = code.match(/[\/\*\*|\n]((\s*?.*?)*?)class/);
+  if (typeDoc.length > 2) {
+    itemType.doc = typeDoc[1].replace(/\*|\n|\/| /g, '');
+  }
+  itemType.name = code.match(/class((\s*?.*?)*?)DTO/g)[0].replace('class ', '');
+
+  var properties = code.match(/\{((\s*?.*?)*?)\}/g);
+  for (var i in properties) {
+
+  }
+
+  return itemType;
 }
