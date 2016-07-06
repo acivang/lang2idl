2446d918501867b51c8527a6d7cb01666bd9954f
Aci <wangz@qiezzi.com>
2016-07-06 18:58:01 +0800

    finish type idl

--- src/groovy2idl.ts
+++ src/groovy2idl.ts
@@ -70,25 +70,25 @@ function getInterface(code: string): any {
   itemInterface.name = code.match(/interface((\s*?.*?)*?)Facade/)[0].replace('interface ', '');
   var interfaceDoc = code.match(/\/\*\*((\s*?.*?)*?)interface/);
   if (interfaceDoc && interfaceDoc.length > 2) {
-    itemInterface.doc = interfaceDoc[1].substring(0,interfaceDoc[1].indexOf('*/')).replace(/\*|\n|\/| |/g, '');
+    itemInterface.doc = interfaceDoc[1].substring(0, interfaceDoc[1].indexOf('*/')).replace(/\*|\n|\/| |/g, '');
   } else {
     throw new Error(`no comment for the interface: ${itemInterface.name}`);
   }
 
   var metaes = code.match(/\*\/((\s*?.*?)*?)interface/);
-  if(metaes){
+  if (metaes) {
     metaes = metaes[0].match(/@((\s*?.*?)*?)\n/g);
   }
   for (var i in metaes) {
     itemInterface.meta.push({
-      type: rpcPackage + metaes[i].replace(/\n|@/g,''),
+      type: rpcPackage + metaes[i].replace(/\n|@/g, ''),
       args: {}
     });
   }
 
   var methodCode = code.match(/\{((\s*?.*?)*?)\}/g);
-  if(!methodCode){
-    throw new Error(`no methods for the interface: ${ itemInterface.name }`);
+  if (!methodCode) {
+    throw new Error(`no methods for the interface: ${itemInterface.name}`);
   }
   var methods = methodCode[0].match(/\/\*\*((\s*?.*?)*?)\)/g);
   for (var i in methods) {
@@ -114,30 +114,30 @@ function getMethod(code: string): any {
   method.args.pop();
 
   var methodName = code.match(/[a-zA-Z](w?.)*\(/);
-  if(!methodName){
-    throw new Error(`code format error: ${ code }`);
+  if (!methodName) {
+    throw new Error(`code format error: ${code}`);
   }
-  method.name = methodName[0].match(/[ ](w?.)*\(/)[0].replace(/ |\(/g,'');
-  method.return.type = methodName[0].match(/[a-zA-Z](w?.)* /)[0].replace(' ','');
+  method.name = methodName[0].match(/[ ](w?.)*\(/)[0].replace(/ |\(/g, '');
+  method.return.type = methodName[0].match(/[a-zA-Z](w?.)* /)[0].replace(' ', '');
 
   var doces = code.match(/\*((\s*?.*?)*?)\n/g)
-  if(!doces){
-    throw new Error(`no document for the method: ${ method.name }`);
+  if (!doces) {
+    throw new Error(`no document for the method: ${method.name}`);
   }
-  method.doc = doces[1].replace(/\* |\n/g,'');
+  method.doc = doces[1].replace(/\* |\n/g, '');
 
   var docReturn = code.match(/\@return((\s*?.*?)*?)\n/);
-  method.return.doc = docReturn[0].replace(/@return |\n/g,'');
+  method.return.doc = docReturn[0].replace(/@return |\n/g, '');
 
   var argsDoc = code.match(/\@param((\s*?.*?)*?)\n/g);
-  var args = code.match(/\(((\s*?.*?)*?)\)/g)[0].replace(/\(|\)/g,'').split(',');
- 
-  var methodArg = {
-    name: '',
-    type: '',
-    doc: ''
-  }
-  for(var i in args){
+  var args = code.match(/\(((\s*?.*?)*?)\)/g)[0].replace(/\(|\)/g, '').split(',');
+
+  for (var i in args) {
+    var methodArg = {
+      name: '',
+      type: '',
+      doc: ''
+    }
     var tmp = args[i].split(' ');
     methodArg.type = tmp[0];
     methodArg.name = tmp[1];
@@ -161,15 +161,32 @@ function getType(code: string): any {
   itemType.properties.pop();
 
   itemType.package = code.match(/package((\s*?.*?)*?)\n/g)[0].replace(/package |\n/g, '');
-  var typeDoc = code.match(/[\/\*\*|\n]((\s*?.*?)*?)class/);
+  var typeDoc = code.match(/\/\*\*((\s*?.*?)*?)class/);
   if (typeDoc.length > 2) {
     itemType.doc = typeDoc[1].replace(/\*|\n|\/| /g, '');
   }
   itemType.name = code.match(/class((\s*?.*?)*?)DTO/g)[0].replace('class ', '');
 
-  var properties = code.match(/\{((\s*?.*?)*?)\}/g);
+  var propertiesTmp = code.match(/\{((\s*?.*?)*?)\}/)[0];
+  var properties = propertiesTmp.match(/[a-zA-Z]((\s*?.*?)*?)\n/g);
+  var propertyDoc = propertiesTmp.match(/\/\*\*((\s*?.*?)*?)\//g);
+  if (!properties) {
+    throw new Error(`no properties for: ${itemType.name}`);
+  }
+  if (!propertyDoc) {
+    throw new Error(`no property's comment for: ${itemType.name}`);
+  }
+  if (properties.length != propertyDoc.length) {
+    throw new Error(`lost some properties comment: ${itemType.name}`);
+  }
   for (var i in properties) {
-
+    var tmp = properties[i].replace('\n', '').split(' ');
+    var property = {
+      name: tmp[1],
+      type: tmp[0],
+      doc: propertyDoc[i].match(/\* ((\s*?.*?)*?)\n/g)[0].replace(/\* |\n/g, '')
+    };
+    itemType.properties.push(property);
   }
 
   return itemType;
