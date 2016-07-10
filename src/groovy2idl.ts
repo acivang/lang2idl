e56da6a3d61d2f46cacfbb790520b4e043b02fed
Aci <wangz@qiezzi.com>
2016-07-10 14:26:32 +0800

    replace var with let

--- src/groovy2idl.ts
+++ src/groovy2idl.ts
@@ -5,36 +5,36 @@ import * as type from './utile/type';
 let rpcPackage: string = 'org.nofdev.rpc.';
 
 export function convert(path: string): void {
-  var isDir: boolean = false;
+  let isDir: boolean = false;
   if (path.lastIndexOf('.groovy') < 0 || path.lastIndexOf('*.groovy') > 0) {
     isDir = true;
   }
   if (!fileStream.existsSync(path)) {
     throw new Error(`no such file or directory, open '${path}'`);
   }
-  var filesPath: string[];
+  let filesPath: string[];
   if (isDir) {
     filesPath = fileStream.readdirSync(path);
   } else {
     filesPath = [path];
   }
-  var idl = { meta: {}, interfaces: [{}], types: [{}] };
+  let idl = { meta: {}, interfaces: [{}], types: [{}] };
   idl.interfaces.pop();
   idl.types.pop();
-  for (var index in filesPath) {
-    var file = filesPath[index];
+  for (let index in filesPath) {
+    let file = filesPath[index];
     if (file.lastIndexOf('DTO.groovy') < 0 && file.lastIndexOf('Facade.groovy') < 0) {
       continue;
     }
-    var code = fileStream.readFileSync(path + file).toString();
+    let code = fileStream.readFileSync(path + file).toString();
     if (file.lastIndexOf('Facade.groovy') > -1) {
 
-      var itemInterface = getInterface(code);
+      let itemInterface = getInterface(code);
       idl.interfaces.push(itemInterface);
 
     } else if (file.lastIndexOf('DTO.groovy') > -1) {
 
-      var itemType = getType(code);
+      let itemType = getType(code);
       idl.types.push(itemType);
     }
 
@@ -44,7 +44,7 @@ export function convert(path: string): void {
 
 function getInterface(code: string): any {
 
-  var itemInterface = {
+  let itemInterface = {
     package: '',
     name: '',
     doc: '',
@@ -56,7 +56,7 @@ function getInterface(code: string): any {
   };
   itemInterface.meta.pop();
   itemInterface.methods.pop();
-  var itemMethod = {
+  let itemMethod = {
     name: '',
     doc: '',
     throws: [{}],
@@ -65,35 +65,35 @@ function getInterface(code: string): any {
   };
   itemMethod.throws.pop();
   itemMethod.args.pop();
-  var dicTypes = [''];
+  let dicTypes = [''];
 
   itemInterface.package = code.match(/package((\s*?.*?)*?)\n/)[0].replace(/package |\n/g, '');
   itemInterface.name = code.match(/interface((\s*?.*?)*?)Facade/)[0].replace('interface ', '');
-  var interfaceDoc = code.match(/\/\*\*((\s*?.*?)*?)interface/);
+  let interfaceDoc = code.match(/\/\*\*((\s*?.*?)*?)interface/);
   if (interfaceDoc && interfaceDoc.length > 2) {
     itemInterface.doc = interfaceDoc[1].substring(0, interfaceDoc[1].indexOf('*/')).replace(/\*|\n|\/| |/g, '');
   } else {
     throw new Error(`no comment for the interface: ${itemInterface.name}`);
   }
 
-  var metaes = code.match(/\*\/((\s*?.*?)*?)interface/);
+  let metaes = code.match(/\*\/((\s*?.*?)*?)interface/);
   if (metaes) {
     metaes = metaes[0].match(/@((\s*?.*?)*?)\n/g);
   }
-  for (var i in metaes) {
+  for (let i in metaes) {
     itemInterface.meta.push({
       type: rpcPackage + metaes[i].replace(/\n|@/g, ''),
       args: {}
     });
   }
 
-  var methodCode = code.match(/\{((\s*?.*?)*?)\}/g);
+  let methodCode = code.match(/\{((\s*?.*?)*?)\}/g);
   if (!methodCode) {
     throw new Error(`no methods for the interface: ${itemInterface.name}`);
   }
-  var methods = methodCode[0].match(/\/\*\*((\s*?.*?)*?)\)/g);
-  for (var i in methods) {
-    var method = getMethod(methods[i], itemInterface.package);
+  let methods = methodCode[0].match(/\/\*\*((\s*?.*?)*?)\)/g);
+  for (let i in methods) {
+    let method = getMethod(methods[i], itemInterface.package);
     itemInterface.methods.push(method);
   }
 
@@ -101,7 +101,7 @@ function getInterface(code: string): any {
 }
 
 function getMethod(code: string, packageName: string): any {
-  var method = {
+  let method = {
     name: '',
     doc: '',
     throws: [{}],
@@ -114,7 +114,7 @@ function getMethod(code: string, packageName: string): any {
 
   method.args.pop();
 
-  var methodName = code.match(/[a-zA-Z](w?.)*\(/);
+  let methodName = code.match(/[a-zA-Z](w?.)*\(/);
   if (!methodName) {
     throw new Error(`code format error: ${code}`);
   }
@@ -122,30 +122,30 @@ function getMethod(code: string, packageName: string): any {
   method.return.type = methodName[0].match(/[a-zA-Z](w?.)* /)[0].replace(' ', '');
   method.return.type = getTypeParam(method.return.type, code, packageName);
 
-  var doces = code.match(/\*((\s*?.*?)*?)\n/g)
+  let doces = code.match(/\*((\s*?.*?)*?)\n/g)
   if (!doces) {
     throw new Error(`no document for the method: ${method.name}`);
   }
   method.doc = doces[1].replace(/\* |\n/g, '');
 
-  var docReturn = code.match(/\@return((\s*?.*?)*?)\n/);
+  let docReturn = code.match(/\@return((\s*?.*?)*?)\n/);
   method.return.doc = docReturn[0].replace(/@return |\n/g, '');
 
-  var argsDoc = code.match(/\@param((\s*?.*?)*?)\n/g);
-  var args = code.match(/\(((\s*?.*?)*?)\)/g)[0].replace(/\(|\)/g, '').split(',');
+  let argsDoc = code.match(/\@param((\s*?.*?)*?)\n/g);
+  let args = code.match(/\(((\s*?.*?)*?)\)/g)[0].replace(/\(|\)/g, '').split(',');
 
-  for (var i in args) {
-    var methodArg: any = {
+  for (let i in args) {
+    let methodArg: any = {
       name: '',
       type: '',
       doc: ''
     }
-    var tmp = args[i].split(' ');
-    var paramType = tmp[0];
+    let tmp = args[i].split(' ');
+    let paramType = tmp[0];
     if (paramType.length === 0) {
       paramType = tmp[1];
     }
-    var argType = getTypeParam(paramType, code);
+    let argType = getTypeParam(paramType, code);
     methodArg.type = argType.type;
     if (argType.typeParams) {
       methodArg.typeParams = argType.typeParams;
@@ -161,7 +161,7 @@ function getMethod(code: string, packageName: string): any {
 
 function getType(code: string): any {
 
-  var itemType = {
+  let itemType = {
     package: '',
     name: '',
     doc: '',
@@ -171,15 +171,15 @@ function getType(code: string): any {
   itemType.properties.pop();
 
   itemType.package = code.match(/package((\s*?.*?)*?)\n/g)[0].replace(/package |\n/g, '');
-  var typeDoc = code.match(/\/\*\*((\s*?.*?)*?)class/);
+  let typeDoc = code.match(/\/\*\*((\s*?.*?)*?)class/);
   if (typeDoc.length > 2) {
     itemType.doc = typeDoc[1].replace(/\*|\n|\/| /g, '');
   }
   itemType.name = code.match(/class((\s*?.*?)*?)DTO/g)[0].replace('class ', '');
 
-  var propertiesTmp = code.match(/\{((\s*?.*?)*?)\}/)[0];
-  var properties = propertiesTmp.match(/[a-zA-Z]((\s*?.*?)*?)\n/g);
-  var propertyDoc = propertiesTmp.match(/\/\*\*((\s*?.*?)*?)\//g);
+  let propertiesTmp = code.match(/\{((\s*?.*?)*?)\}/)[0];
+  let properties = propertiesTmp.match(/[a-zA-Z]((\s*?.*?)*?)\n/g);
+  let propertyDoc = propertiesTmp.match(/\/\*\*((\s*?.*?)*?)\//g);
   if (!properties) {
     throw new Error(`no properties for: ${itemType.name}`);
   }
@@ -189,11 +189,11 @@ function getType(code: string): any {
   if (properties.length != propertyDoc.length) {
     throw new Error(`lost some properties comment: ${itemType.name}`);
   }
-  for (var i in properties) {
-    var property = {};
-    var tmp = properties[i].replace('\n', '').split(' ');
+  for (let i in properties) {
+    let property = {};
+    let tmp = properties[i].replace('\n', '').split(' ');
 
-    var typeParam = getTypeParam(tmp[0], code);
+    let typeParam = getTypeParam(tmp[0], code);
 
     property = {
       name: tmp[1],
@@ -208,16 +208,16 @@ function getType(code: string): any {
 }
 
 function getTypeParam(typeCode: string, code: string, packageName?: string): any {
-  var propType = type.toIdlType(typeCode);
-  var propTypeParams = [""];
+  let propType = type.toIdlType(typeCode);
+  let propTypeParams = [""];
   propTypeParams.pop();
   if (!propType) {//无类型或非数据类型，非数据类型需要处理
     if (typeCode.indexOf('<') > -1) {
-      var typeTmp = typeCode.split('<');
+      let typeTmp = typeCode.split('<');
       propType = typeTmp[0];
       typeTmp[1] = typeTmp[1].replace(/\>| /g, '');
       if (typeTmp[1].indexOf(',') > -1) {
-        var tmpPropTypeParams = typeTmp[1].split(',');
+        let tmpPropTypeParams = typeTmp[1].split(',');
         propTypeParams.push(getTypeWithPackage(tmpPropTypeParams[0], code, packageName));
         propTypeParams.push(getTypeWithPackage(tmpPropTypeParams[1], code, packageName));
       }
@@ -244,8 +244,8 @@ function getTypeParam(typeCode: string, code: string, packageName?: string): any
 
 function getTypeWithPackage(type: string, code: string, packageName: string): string {
 
-  var packageReg = new RegExp(`import ((\s*?.*?)*?)${type}`);
-  var thisPackage = code.match(packageReg);
+  let packageReg = new RegExp(`import ((\s*?.*?)*?)${type}`);
+  let thisPackage = code.match(packageReg);
   if (thisPackage) {
     return thisPackage[0].split(' ')[1];
   }
--- src/utile/type.ts
+++ src/utile/type.ts
@@ -1,28 +1,28 @@
 
-var IdlTypeDictionary: { [type: string]: string} = {
-  ['float']:'decimal',
-  ['double']:'decimal',
-  ['int']:'number',
-  ['int32']:'number',
-  ['int64']:'number',
-  ['integer']:'number',
-  ['biginteger']:'number',
-  ['bigdecimal']:'number',
-  ['byte']:'number',
-  ['short']:'number',
-  ['long']:'number',
-  ['string']:'string',
-  ['char']:'string',
-  ['boolean']:'boolean',
-  ['offsetdatetime']:'date',
-  ['datetime']:'Date',
-  ['list']:'array',
+let IdlTypeDictionary: { [type: string]: string } = {
+  ['float']: 'decimal',
+  ['double']: 'decimal',
+  ['int']: 'number',
+  ['int32']: 'number',
+  ['int64']: 'number',
+  ['integer']: 'number',
+  ['biginteger']: 'number',
+  ['bigdecimal']: 'number',
+  ['byte']: 'number',
+  ['short']: 'number',
+  ['long']: 'number',
+  ['string']: 'string',
+  ['char']: 'string',
+  ['boolean']: 'boolean',
+  ['offsetdatetime']: 'date',
+  ['datetime']: 'Date',
+  ['list']: 'array',
   ['void']: 'viod',
   ['def']: 'void'
 };
 
-export function toIdlType(type:string): string{
-  var idlType = 'undefined';
+export function toIdlType(type: string): string {
+  let idlType = 'undefined';
   idlType = IdlTypeDictionary[type.toLowerCase()];
   return idlType;
 }
